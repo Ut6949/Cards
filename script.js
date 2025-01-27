991d4548f03f85f3e3fccbafcaf16932a0a8dc0a
Uzair Tariq <uzairtariq6949@gmail.com>
2025-01-28 01:45:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 allProducts = document.getElementById("allProducts");
 let products = [
     {id:"1", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"1", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"1", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"1", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"1", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""}];
+    {id:"2", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"3", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"4", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"5", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"6", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"7", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"8", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"9", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"10", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"11", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"12", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"13", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"14", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"15", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"16", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"17", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"18", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"19", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"20", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"21", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"22", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"23", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"24", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"25", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""}];
 for (var i=0; i < products.length; i++) {
 allProducts.innerHTML += `
         <div class="col-xl-3 col-lg-3 col-md-4 col-sm- col-12">
