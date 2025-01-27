a439b8e000b15eb07b7f7499d8817464904b0ab5
Uzair Tariq <uzairtariq6949@gmail.com>
2025-01-28 03:05:47 +0500

    Images added

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 allProducts = document.getElementById("allProducts");
 let products = [
-    {id:"1", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"2", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"3", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"4", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"5", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"6", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"7", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"8", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"9", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"10", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"11", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"12", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"13", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"14", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"15", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"16", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"17", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"18", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"19", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"20", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"21", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"22", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"23", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"24", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
-    {id:"25", image:"", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""}];
+    {id:"1", image:"Images/1.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"2", image:"Images/2.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"3", image:"Images/3.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"4", image:"Images/4.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"5", image:"Images/5.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"6", image:"Images/6.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"7", image:"Images/7.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"8", image:"Images/8.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"9", image:"Images/9.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"10", image:"Images/10.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"11", image:"Images/11.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"12", image:"Images/12.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"13", image:"Images/13.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"14", image:"Images/14.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"15", image:"Images/15.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"16", image:"Images/16.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"17", image:"Images/17.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"18", image:"Images/18.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"19", image:"Images/19.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"20", image:"Images/20.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"21", image:"Images/21.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"22", image:"Images/22.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"23", image:"Images/23.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"24", image:"Images/24.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""},
+    {id:"25", image:"Images/25.png", name:"Wild Property",ptype:"", psize:"",pdetail:"",pprice:""}];
 for (var i=0; i < products.length; i++) {
 allProducts.innerHTML += `
         <div class="col-xl-3 col-lg-3 col-md-4 col-sm- col-12">
@@ -38,7 +38,7 @@ allProducts.innerHTML += `
                 <p class="card-text">
                   ${products[i].pdetail}
                 </p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-primary">Details</a>
               </div>
             
         </div>
